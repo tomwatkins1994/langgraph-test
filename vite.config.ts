d79d77bf933dd161c98886dc41c0a7068046e218
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-15 22:56:38 +0000

    Set up env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+        include: ["src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+        setupFiles: ["dotenv/config"],
     },
 });
