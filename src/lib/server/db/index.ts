e9e185a2a415042c7758df8889519cd183c36bf3
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-16 16:28:07 +0000

    Export schema

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { env } from "$env/dynamic/private";
+import * as schema from "./schema";
 
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
-export const db = drizzle(env.DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL, { schema });
+export * as schema from "./schema";
