27b09991ec266c57db413796a23b6a9017e52fe0
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-16 01:06:02 +0000

    Basic svelte app

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-    test: {
-        include: ["src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
-        setupFiles: ["dotenv/config"],
-    },
-});
