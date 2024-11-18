063d43caf3dd15d52bfa7b7a895f157465a1f507
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-18 23:45:01 +0000

    Test web search when it shouldn't find anything

--- src/lib/server/ai/graphs/execify.test.ts
+++ src/lib/server/ai/graphs/execify.test.ts
@@ -35,20 +35,38 @@ describe.concurrent("Execify Graph", () => {
         "Should answer from the web",
         { timeout: 20_000 },
         async ({ thread }) => {
-            const question =
-                "What is the most recent community event Execify was part of?";
+            const question = "What is the Execify exec dashboard?";
             const state = await execifyGraph.invoke(
                 {
                     messages: [new HumanMessage(question)],
                 },
                 { configurable: { thread_id: thread.id } }
             );
-
             expect(state.webSearched).toBe(true);
 
             const answer = state.messages[state.messages.length - 1];
             const { score } = await gradeAnswer(question, answer);
             expect(score).toBe("yes");
+        }
+    );
+
+    aiTest(
+        "Should not find an answer",
+        { timeout: 20_000 },
+        async ({ thread }) => {
+            const question = "How many employees does execify currently have?";
+            const state = await execifyGraph.invoke(
+                {
+                    messages: [new HumanMessage(question)],
+                },
+                { configurable: { thread_id: thread.id } }
+            );
+            expect(state.webSearched).toBe(true);
+            expect(state.relevantDocuments.length).toBe(0);
+
+            const answer = state.messages[state.messages.length - 1];
+            const { score } = await gradeAnswer(question, answer);
+            expect(score).toBe("no");
         }
     );
 });
