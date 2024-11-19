71a145ef9ed213e0d56ed6fee53fb4b4293056ac
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-19 20:49:29 +0000

    Await response

--- src/lib/server/ai/tools/web-search.ts
+++ src/lib/server/ai/tools/web-search.ts
@@ -1,22 +1,23 @@
 import { env } from "$env/dynamic/private";
 import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { tool } from "@langchain/core/tools";
+import { z } from "zod";
 
-export const webSearchTool = new TavilySearchResults({
+export const tavilySearch = new TavilySearchResults({
     maxResults: 2,
     apiKey: env.TAVILY_API_KEY,
 });
 
-// export const webSearchTool = tool(
-//     async ({ query }) => {
-//         const results = tavilySearch.invoke({ query });
-//         console.log({ query, searchResults: results });
-//         return results;
-//     },
-//     {
-//         name: "web_search",
-//         description: "Search the web for the answer to a question.",
-//         schema: z.object({
-//             query: z.string().describe("The query to use in your search."),
-//         }),
-//     }
-// );
+export const webSearchTool = tool(
+    async ({ query }) => {
+        const results = await tavilySearch.invoke({ query });
+        return results;
+    },
+    {
+        name: "web_search",
+        description: "Search the web for the answer to a question.",
+        schema: z.object({
+            query: z.string().describe("The query to use in your search."),
+        }),
+    }
+);
