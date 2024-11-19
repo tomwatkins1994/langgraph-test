ffe6f2fb3387a1680213a421b8bcf4b224a2466f
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-19 23:45:37 +0000

    Use tools condition helper

--- src/lib/server/ai/graphs/execify-with-tools.ts
+++ src/lib/server/ai/graphs/execify-with-tools.ts
@@ -1,5 +1,5 @@
 import { SystemMessage } from "@langchain/core/messages";
-import type { AIMessage, BaseMessage } from "@langchain/core/messages";
+import type { BaseMessage } from "@langchain/core/messages";
 import { ChatOpenAI } from "@langchain/openai";
 import { END, START, StateGraph } from "@langchain/langgraph";
 import { Annotation } from "@langchain/langgraph";
@@ -7,7 +7,7 @@ import { env } from "$env/dynamic/private";
 import { pgCheckpointer } from "../pg-peristance";
 import { setupLangsmith } from "../utils/setup-langsmith";
 import { webSearchTool } from "../tools/web-search";
-import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { ToolNode, toolsCondition } from "@langchain/langgraph/prebuilt";
 import { pdfSearchTool } from "../tools/pdf-search";
 import { convertToOpenAITool } from "@langchain/core/utils/function_calling";
 
@@ -53,26 +53,13 @@ async function generateNode(
     return { messages: [response] };
 }
 
-// Conditonal Edges
-
-function continueOrUseTool(state: typeof StateAnnotation.State) {
-    const messages = state.messages;
-    const lastMessage = messages[messages.length - 1] as AIMessage;
-
-    if (lastMessage.tool_calls?.length) {
-        return "tools";
-    }
-
-    return "__end__";
-}
-
 // Workflow
 
 const workflow = new StateGraph(StateAnnotation)
     .addNode("generate", generateNode)
     .addNode("tools", toolNode)
     .addEdge(START, "generate")
-    .addConditionalEdges("generate", continueOrUseTool)
+    .addConditionalEdges("generate", toolsCondition)
     .addEdge("tools", "generate")
     .addEdge("generate", END);
 
