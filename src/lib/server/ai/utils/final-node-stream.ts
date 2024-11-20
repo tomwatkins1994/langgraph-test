f67e8b32d731ccd75842e36ed1c4e973e81b74cc
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-20 12:44:35 +0000

    Comment out

--- src/lib/server/ai/utils/final-node-stream.ts
+++ src/lib/server/ai/utils/final-node-stream.ts
@@ -1,24 +1,5 @@
 import type { StreamEvent } from "@langchain/core/tracers/log_stream";
 
-const transformContent = {
-    start() {},
-    async transform(
-        chunk: StreamEvent,
-        controller: TransformStreamDefaultController
-    ) {
-        if (chunk === null) {
-            controller.terminate();
-            return;
-        }
-        const { event, tags, data } = chunk;
-        if (event === "on_chat_model_stream") {
-            if (data.chunk.content && tags?.includes("final_node")) {
-                controller.enqueue(data.chunk.content);
-            }
-        }
-    },
-};
-
 export class FinalNodeStream extends TransformStream {
     constructor() {
         super({
@@ -41,3 +22,22 @@ export class FinalNodeStream extends TransformStream {
         });
     }
 }
+
+// const transformContent = {
+//     start() {},
+//     async transform(
+//         chunk: StreamEvent,
+//         controller: TransformStreamDefaultController
+//     ) {
+//         if (chunk === null) {
+//             controller.terminate();
+//             return;
+//         }
+//         const { event, tags, data } = chunk;
+//         if (event === "on_chat_model_stream") {
+//             if (data.chunk.content && tags?.includes("final_node")) {
+//                 controller.enqueue(data.chunk.content);
+//             }
+//         }
+//     },
+// };
